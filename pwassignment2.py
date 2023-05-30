7ae334165e119bd94391f0cee5bb3b8f320ff095
nitishsingh6277 <116247310+nitishsingh6277@users.noreply.github.com>
2023-05-30 11:06:05 +0530

    Create pwassignment2.py

---
+++ pwassignment2.py
@@ -0,0 +1,71 @@
+      #'''Q1. To comment a code we use # for one line comments .. to comment multiple lines of comment we do is 
+we use the three colon line as in this first question answer i am using.. 
+in code also inside function we can mention the doc string using three colon line as in this case we have done..
+'''
+"we can also comment using single quote marks as in this examples "
+"""we can also comment using double quote marks as mentioned...
+so there are four ways.. 
+1. #  
+2. ""(single quotes )
+3. ""double quotes"" 
+4. '''doc strings...''', """
+
+
+#Q2.
+'''variables are names to the placeholder for the memory location where we have integer float or string or any other types of data we can store, 
+such as n = 5 so n is name which points to memory location where 5 is stored .. similarly, we have 
+n1 = 3.5567 or stringsss = 'pw'  
+we also use the term object for the data .. to which these variables are the refrences.'''
+"in python we declare the variables using simple name of variables and assignment operators, "
+"say n = 5.5 later we can assign some other types of data to the same varibles.. "
+"like n = 'pwskills' and n = 2 so first float then string and then integers values so whatever varibles we need we can declare immediately and assign values or take the inputs from the user"
+
+#Q3.
+#to convert to any other data type we just have to use python built in function with that data type names.. 
+x = 5
+y = float(x) #convert to float
+z1 = str(x) #conver to string..
+print(x, y, z1)
+
+
+
+#Q4.
+'''first of create a file say in vs code and then write the code as i m writing it,
+then save it and then go to that path like where the file is stored and then 
+execute the command py .\nameofthefile.py so the code in the given file will start 
+executing immmediately....'''
+
+#Q5.
+list1 = [1, 2, 3, 4, 5]
+print(list1[1:3])
+#Q6
+'''complex numbers are basically combination of real numbers and imaginary numbers we can 
+represent complex no in python as numbr = 5+5j where 5 is real no while the 5j is imaginary no..
+'''
+complex1 = 4 + 6j
+complex2 = 5 + 80j
+print(complex1)
+
+#Q7..
+age = 25 
+#we hve declare age as variable and 25 is integer data object and age is refrencing or point to that object.. 
+
+#Q8
+price = 9.99
+print(type(price))
+#type is of float 
+
+#Q9
+name = "NItish kumar singh"
+print(name)
+#Q10
+str1 = "Hello World!"
+print(str1[6:11])
+
+#Q11
+is_student = True #this indicates yes is_student is true ..
+is_student = False #this indicates not a student.. boolean varialbes
+
+
+
+
